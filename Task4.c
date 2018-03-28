70f17035061c537cca4b769de0b710ad39816219
Melanie <melanie.salzer@uzh.ch>
2018-03-28 14:50:30 +0200

    add task

---
+++ Task4.c
@@ -0,0 +1,37 @@
+
+struct node{
+	int val;
+	struct node* next;
+};
+
+
+struct list{
+	struct node* head;
+};
+
+void append(struct list * list A, int val){
+	
+}
+
+void reverse(struct list * list A){
+	
+}
+
+void clear(struct list * list A){
+	
+}
+
+void print(struct list * list A){
+	
+}
+
+void deleteAll(struct list * list A, int i){
+	
+}
+void deleteLast(struct list * list A){
+	
+}
+
+void max(struct list * list A){
+	
+}
