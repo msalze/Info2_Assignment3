d0ca0d1e923a94d6fcfd84942a6de229acabf48a
Melanie <melanie.salzer@uzh.ch>
2018-03-28 14:25:09 +0200

    Signed-off-by: Melanie <melanie.salzer@uzh.ch>

--- Task2.c
+++ Task2.c
@@ -7,32 +7,26 @@ void swap(int A[], int i, int j){
 }
 
 int partitionHoare(int A[], int low, int high){
-	if (low == high){
-		return low;
-	}
 	int x = A[high];
-	int i = low;
-	int j = high-1;
-	while (1){
-		while (A[j]>x && j>=i){
-			j=j-1;
-		}
-		while (A[i]<x && i<j){
-			i=i+1;
-		}
-		if (i<j){
-			swap(A,i,j);
-		}
-		else {
-			return i;
-		}
-	}
+	int i = low-1;
+	int j = high+1;
+	while (1) {
+        do  j--; while (A[j] > x);
+        do  i++; while (A[i] < x);
+
+        if  (i < j){
+            swap(A,i,j);
+        }
+        else{
+            return j+1;
+        }
+    }
 }
 
 void quicksort(int A[], int low, int high){
 	if (low < high){
 		int m = partitionHoare(A,low,high);
-		/*quicksort(A,low,m);*/
+		quicksort(A,low,m);
 		quicksort(A,m+1,high);
 	}
 }
